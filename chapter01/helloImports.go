47f2a547f1dfe5a24e36687e8a1c756f5343d3af
felipejsm <felipejsm41@gmail.com>
2020-06-10 15:02:38 -0300

    Trabalhando com strings no printl

---
+++ chapter01/helloImports.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strings"
+)
+
+func main() {
+	fmt.Println(math.Floor(2.75))
+	fmt.Println(strings.Title("server para capitalizar a frase?"))
+	fmt.Println("You're watching a master @ work\n(Yeah)")
+	fmt.Println("This is what you all been waitin' for ain't it? \t Rap Game homie, I been waitin' for the payment")
+
+}
---
+++ chapter01/helloStrings.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Letra\tRUNES")
+	fmt.Print("A\t")
+	fmt.Print('A')
+}
