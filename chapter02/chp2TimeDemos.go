a0fe226e201599bff67822cd5efbb61c1d72547b
felipejsm <felipejsm41@gmail.com>
2020-06-13 16:15:51 -0300

    Chapter 2 initial commit

---
+++ chapter02/chp2TimeDemos.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	jayZBirthDate := time.Date(1969, 12, 4, 12, 3, 3, 2, time.UTC)
+
+	fmt.Println("Jay has ", time.Since(jayZBirthDate))
+}
