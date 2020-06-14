554bf1ec05a7e9832629c3fe7baeb92200c05f4f
felipejsm <felipejsm41@gmail.com>
2020-06-14 09:58:00 -0300

    Chapter 2 progress

--- chapter02/chp2TimeDemos.go
+++ chapter02/chp2TimeDemos.go
@@ -9,4 +9,5 @@ func main() {
 	jayZBirthDate := time.Date(1969, 12, 4, 12, 3, 3, 2, time.UTC)
 
 	fmt.Println("Jay has ", time.Since(jayZBirthDate))
+	fmt.Println("Now sub Jay-z's birthdate", time.Now().Sub(jayZBirthDate).)
 }
