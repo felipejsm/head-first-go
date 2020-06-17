e63a00bf54707d1c0fc89ea27dd39f4398ee2724
felipejsm <felipejsm41@gmail.com>
2020-06-17 07:47:36 -0300

    Guess Game

---
+++ chapter02/guess3.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	seconds := time.Now().Unix()
+	rand.Seed(seconds)
+	target := rand.Intn(100) + 1
+	fmt.Println("I've chosen a random numbere between 1 and 100")
+	fmt.Println("Can you guess it?")
+	fmt.Println(target)
+
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	input := strings.TrimSpace(input)
+	guess, err := strconv.Atoi(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
