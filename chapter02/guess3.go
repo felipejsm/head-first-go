0c3076c56ee926aeaaf3fcff54be4868e159edb5
felipejsm <felipejsm41@gmail.com>
2020-06-18 08:56:00 -0300

    Guess game completed

--- chapter02/guess3.go
+++ chapter02/guess3.go
@@ -17,24 +17,32 @@ func main() {
 	target := rand.Intn(100) + 1
 	fmt.Println("I've chosen a random numbere between 1 and 100")
 	fmt.Println("Can you guess it?")
-	fmt.Println(target)
 
 	reader := bufio.NewReader(os.Stdin)
-	input, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
-	}
-	input = strings.TrimSpace(input)
-	guess, err := strconv.Atoi(input)
-	if err != nil {
-		log.Fatal(err)
-	}
+	success := false
+	for guess := 0; guess < 10; guess++ {
+		fmt.Println("You have", 10-guess, "guesses left.")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		input = strings.TrimSpace(input)
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	if guess < target {
-		fmt.Println("low")
-	} else if guess > target {
-		fmt.Println("high")
-	} else {
-		fmt.Println("Bull's eye")
+		if guess < target {
+			fmt.Println("low")
+		} else if guess > target {
+			fmt.Println("high")
+		} else {
+			fmt.Println("Bull's eye")
+			success = true
+			break
+		}
+	}
+	if !success {
+		fmt.Println("Sorry, you didn't guess my number. It was: ", target)
 	}
 }
