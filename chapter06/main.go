fc81a3c9d5d31c9c3fb8233a718bbcd4f8886c7e
felipejsm <felipejsm41@gmail.com>
2020-07-03 00:21:40 -0300

    Average using slice

---
+++ chapter06/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/headfirstgo/datafile"
+)
+
+func main() {
+	numbers, err := datafile.GetFloats("C:\\moses-learning\\head-first-go\\chapter06\\data.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var sum float64 = 0
+	for _, number := range numbers {
+		sum += number
+	}
+	sample := float64(len(numbers))
+	fmt.Println("Sum: ", sum)
+	fmt.Printf("Average: %0.2f\n", sum/sample)
+}
