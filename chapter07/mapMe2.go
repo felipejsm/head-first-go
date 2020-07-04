b1cd5e01d76c8a4de8562096209d6f45be3c69b1
felipejsm <felipejsm41@gmail.com>
2020-07-03 23:50:28 -0300

    comma ok idiom

---
+++ chapter07/mapMe2.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// How to tell zero values apart from assigned values
+func main() {
+	counters := map[string]int{"a": 3, "b": 0}
+	var value int
+	// comma ok idiom -> used to distinguish from a 'found' and 'not found' thing
+	var ok bool
+	value, ok = counters["a"]
+	fmt.Println(value, ok)
+	value, ok = counters["b"]
+	fmt.Println(value, ok)
+	value, ok = counters["c"]
+	fmt.Println(value, ok)
+}
