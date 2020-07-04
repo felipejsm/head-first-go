d698029f3fca79078e06cc1e48be4409d8e20596
felipejsm <felipejsm41@gmail.com>
2020-07-03 23:43:58 -0300

    Some exercises with maps

--- chapter07/mapMe.go
+++ chapter07/mapMe.go
@@ -11,4 +11,18 @@ func main() {
 	for rapper, _ := range rappers {
 		fmt.Printf("%s is %d old\n", rapper, rappers[rapper])
 	}
+	emos := make(map[string]int)
+	emos["Jeremy Bolm"] = 40
+	emos["Jesse Lacey"] = 42
+	emos["Mike Kinsella"] = 43
+
+	for emo, _ := range emos {
+		fmt.Printf("%s is %d old\n", emo, emos[emo])
+	}
+
+	danceGavinDanceVocalists := map[string]int{"Kurt Travis": 2010, "Jonny Craig": 2012, "Tilian Pearson": 2020}
+
+	for dgd, _ := range danceGavinDanceVocalists {
+		fmt.Printf("%s left DGD at %d\n", dgd, danceGavinDanceVocalists[dgd])
+	}
 }
